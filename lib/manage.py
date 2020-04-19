a2da3df28352390de2206f152a57c47c86337046
Gaffer Fitch <gaffer.fitch@gmail.com>
2020-04-19 12:38:21 -0400

    Handling new settings

--- lib/manage.py
+++ lib/manage.py
@@ -3,6 +3,7 @@ import yaml
 import requests
 import platform
 import calendar
+import operator
 import functools
 import subprocess
 
@@ -31,7 +32,6 @@ def app():
     api.add_resource(PodRD, '/pod/<string:pod>')
     api.add_resource(AppLP, '/app')
     api.add_resource(AppRIU, '/app/<string:name>')
-    api.add_resource(Label, '/label')
 
     return app
 
@@ -638,6 +638,15 @@ class App(flask_restful.Resource):
             "actions": []
         }
 
+        if "url" in obj:
+            app["url"] = obj["url"]
+
+        if "settings" in obj:
+            app["settings"] = obj["settings"]
+
+        if "settings" in obj.get("spec", {}):
+            app["actions"].append("Settings")
+
         if app["status"] in ["Defined"] and app["action"] not in ["Download", "Install"]:
             app["actions"].append("Download")
 
@@ -650,22 +659,12 @@ class App(flask_restful.Resource):
         if app["status"] in ["Installed"] and app["action"] not in ["Uninstall"]:
             app["actions"].append("Uninstall")
 
-        if "labels" in obj.get("spec", {}):
-            app["labels"] = obj["spec"]["labels"]
-
-        if "url" in obj:
-            app["url"] = obj["url"]
-
         if not short:
 
             if "error" in obj:
                 app["error"] = obj["error"]
 
-            if "source" in obj["spec"]:
-                app["source"] = obj["spec"]["source"]
-
-            if "resources" in obj:
-                app["resources"] = obj["resources"]
+            app["yaml"] = yaml.safe_dump(obj, default_flow_style=False)
 
         return app
 
@@ -711,6 +710,48 @@ class AppLP(App):
 
 class AppRIU(App):
 
+    @staticmethod
+    def fields(obj, values):
+
+        fields = opengui.Fields(values, obj.get("settings", {}), obj["spec"].get("settings", []))
+
+        nodes = []
+
+        if [True for field in fields if field.content.get("node")]:
+
+            for node in [node.obj for node in pykube.Node.objects(kube()).filter()]:
+                nodes.append({
+                    "name": node["metadata"]["name"],
+                    "role": "Master" if node["metadata"]["name"] == platform.node() else "Worker",
+                    "labels": node["metadata"]["labels"]
+                })
+
+            nodes.sort(key=operator.itemgetter('role', 'name'))
+
+            for field in fields:
+
+                if not field.content.get("node"):
+                    continue
+
+                field.options = []
+
+                if field.multi:
+                    field.original = []
+
+                for node in nodes:
+
+                    field.options.append(node["name"])
+
+                    label = f"{obj['metadata']['name']}/{field.name}"
+
+                    if node["labels"].get(label) == field.content["node"]:
+                        if field.mulit:
+                            field.original.append(node["name"])
+                        else:
+                            field.original = node["name"]
+
+        return fields
+
     @require_auth
     @require_kube
     def get(self, name):
@@ -734,6 +775,75 @@ class AppRIU(App):
 
         return {self.singular: self.to_dict(obj)}
 
+    @require_auth
+    @require_kube
+    def options(self, name):
+
+        obj = pykube.KlotIOApp.objects(kube()).filter().get(name=name).obj
+
+        values = flask.request.json.get("values", obj.get("settings", {}))
+
+        fields = self.fields(obj, values)
+
+        if values and not fields.validate():
+            return {"fields": fields.to_list(), "errors": fields.errors}
+        else:
+            return {"fields": fields.to_list()}
+
+    @require_auth
+    @require_kube
+    def put(self, name):
+
+        if "values" not in flask.request.json:
+            return {"error": "missing config"}, 400
+
+        obj = pykube.KlotIOApp.objects(kube()).filter().get(name=name).obj
+
+        fields = self.fields(obj, flask.request.json["values"])
+
+        if not fields.validate():
+            return {"fields": fields.to_list(), "errors": fields.errors}, 400
+
+        obj["settings"] = fields.values
+
+        for field in fields:
+
+            if not field.content.get("node") or field.value == field.original:
+                continue
+
+            label = f"{obj['metadata']['name']}/{field.name}"
+
+            current = field.value if field.multi else [field.value]
+            original = field.original if field.multi else [field.original]
+
+            for value in current:
+                if value not in original:
+                    node = pykube.Node.objects(kube()).get(name=value).obj
+                    node["metadata"]["labels"][label] = field.content["node"]
+                    pykube.Node(kube(), node).replace()
+
+            for value in original:
+                if value not in current:
+                    node = pykube.Node.objects(kube()).get(name=value).obj
+                    del node["metadata"]["labels"][label]
+                    pykube.Node(kube(), node).replace()
+
+        if obj["status"] == "NeedSettings":
+            obj["status"] = "Installing"
+
+        pykube.KlotIOApp(kube(), obj).replace()
+
+        try:
+
+            config = pykube.ConfigMap.objects(kube()).filter(namespace=obj["spec"]["namespace"]).get(name="config").obj
+            config["data"] = {"settings.yaml": yaml.safe_dump(flask.request.json["values"])}
+            pykube.ConfigMap(kube(), config).replace()
+
+        except pykube.ObjectDoesNotExist:
+
+            pass
+
+        return {"values": flask.request.json["values"]}
 
     @require_auth
     @require_kube
@@ -747,130 +857,3 @@ class AppRIU(App):
         pykube.KlotIOApp(kube(), obj).delete()
 
         return {"message": f"{name} deleted"}, 201
-
-
-class Label(flask_restful.Resource):
-
-    singular = "label"
-    plural = "labels"
-
-    @require_auth
-    @require_kube
-    def get(self):
-
-        labels = []
-
-        app_filter = {}
-
-        if "app" in flask.request.args:
-            app_filter["field_selector"] = {"metadata.name": flask.request.args["app"]}
-
-        node_filter = {}
-
-        if "node" in flask.request.args:
-            node_filter["field_selector"] = {"metadata.name": flask.request.args["node"]}
-
-        for obj in [app.obj for app in pykube.KlotIOApp.objects(kube()).filter(**app_filter)]:
-            if "labels" in obj["spec"]:
-                for label in obj["spec"]["labels"]:
-                    labels.append({
-                        "app": obj["metadata"]["name"],
-                        "name": label["name"],
-                        "value": label["value"],
-                        "description": label["description"],
-                        "master": label["master"] if "master" in label else False,
-                        "nodes": []
-                    })
-
-        for obj in [node.obj for node in pykube.Node.objects(kube()).filter(**node_filter)]:
-            if "labels" in obj["metadata"]:
-                for node_label in obj["metadata"]["labels"]:
-                    for app_label in labels:
-                        if (
-                            node_label == f"{app_label['app']}/{app_label['name']}" and
-                            obj["metadata"]["labels"][node_label] == app_label["value"]
-                        ):
-                            app_label["nodes"].append(obj["metadata"]["name"])
-
-        return {self.plural: sorted(labels, key=lambda label: f"{label['app']}/{label['name']}={label['value']}")}
-
-    @require_auth
-    @require_kube
-    def post(self):
-
-        if self.singular not in flask.request.json:
-            return {"error": f"missing {self.singular}"}, 400
-
-        label = flask.request.json[self.singular]
-
-        errors = []
-
-        for field in ["app", "name", "value", "node"]:
-            if field not in label:
-                errors.append(f"missing {self.singular}.{field}")
-
-        if errors:
-            return {"errors": errors}, 400
-
-        app_labels = {}
-
-        for obj in [app.obj for app in pykube.KlotIOApp.objects(kube()).filter()]:
-            if "labels" in obj["spec"]:
-                for app_label in obj["spec"]["labels"]:
-                    app_labels[f"{obj['metadata']['name']}/{app_label['name']}={app_label['value']}"] = app_label
-
-        if f"{label['app']}/{label['name']}={label['value']}" not in app_labels:
-            return {"error": f"invalid label {label['app']}/{label['name']}={label['value']}"}, 400
-
-        app_label = app_labels[f"{label['app']}/{label['name']}={label['value']}"]
-
-        obj = pykube.Node.objects(kube()).filter().get(name=flask.request.json[self.singular]["node"]).obj
-
-        if obj["metadata"]["name"] == platform.node() and ("master" not in app_label or not app_label["master"]):
-            return {"error": f"can't label master with {label['app']}/{label['name']}={label['value']}"}, 400
-
-        if "labels" not in obj["metadata"]:
-            obj["metadata"]["labels"] = {}
-
-        obj["metadata"]["labels"][f"{label['app']}/{label['name']}"] = label["value"]
-
-        pykube.Node(kube(), obj).replace()
-
-        return {"message": f"{label['node']} labeled {label['app']}/{label['name']}={label['value']}"}
-
-    @require_auth
-    @require_kube
-    def delete(self):
-
-        if self.singular not in flask.request.json:
-            return {"error": f"missing {self.singular}"}, 400
-
-        label = flask.request.json[self.singular]
-
-        errors = []
-
-        for field in ["app", "name", "node"]:
-            if field not in label:
-                errors.append(f"missing {self.singular,}.{field}")
-
-        if errors:
-            return {"errors": errors}, 400
-
-        app_labels = []
-
-        for obj in [app.obj for app in pykube.KlotIOApp.objects(kube()).filter()]:
-            if "labels" in obj["spec"]:
-                for app_label in obj["spec"]["labels"]:
-                    app_labels.append(f"{obj['metadata']['name']}/{app_label['name']}")
-
-        if f"{label['app']}/{label['name']}" not in app_labels:
-            return {"error": f"invalid label {label['app']}/{label['name']}"}, 400
-
-        obj = pykube.Node.objects(kube()).filter().get(name=flask.request.json[self.singular]["node"]).obj
-
-        if "labels" in obj["metadata"] and f"{label['app']}/{label['name']}" in obj["metadata"]["labels"]:
-            del obj["metadata"]["labels"][f"{label['app']}/{label['name']}"]
-
-        pykube.Node(kube(), obj).replace()
-
-        return {"message": f"{label['node']} unlabeled {label['app']}/{label['name']}"}
