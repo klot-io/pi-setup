9ba9a0b08728c7670aa64688497a8234945ac6cf
Gaffer Fitch <gaffer.fitch@gmail.com>
2020-07-10 12:06:16 -0400

    Better updating

--- bin/export.sh
+++ bin/export.sh
@@ -10,7 +10,7 @@ DEVICEPATH=/dev/$(diskutil list | grep FDisk_partition_scheme | awk '{ print $5 
 
 sudo dd if=${DEVICEPATH} of=./images/pi-$VERSION.img bs=4m & pid=$!
 while sudo kill -0 $pid 2> /dev/null; do
-    sudo kill -s INFO $pid
+    sudo killall -INFO dd
     sleep 5
 done
 sudo chown "$USER" ./images/pi-$VERSION.img
